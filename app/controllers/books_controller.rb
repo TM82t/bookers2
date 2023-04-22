9cb319b765bdd2ed878d8aae6914e87246c0f1b7
EC2 Default User <ec2-user@ip-172-31-5-135.ap-northeast-1.compute.internal>
2023-04-22 12:25:26 +0000

    [Add]4-25 header,footerのレイアウト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,18 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "Book was successfully created"
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render("books/index")
+    end
+  end
+
   def index
   end
 
